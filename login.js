195c737a27892f743246f794bf4c469d45bdc30d
shanningtatum <94034582+shanningtatum@users.noreply.github.com>
2021-11-23 03:10:38 -0500

    Add files via upload

--- login.js
+++ login.js
@@ -3,20 +3,24 @@ const accounts = [
         FirstName: "Naomi",
         username: "naomisong",
         password: "1234",
+        job: "mechanic",
     },
     {
         FirstName: "Oliver",
         username: "oliverray",
-        password: "5678",
+        password: "donkey",
+        job: "burgershot",
     },
     {
         FirstName: "Christopher",
         username: "rigatoni",
         password: "faggio",
+        job: "burgershot",
     },
     {
         FirstName: "Bryan",
         username: "bdogadoo",
         password: "dogadoo",
+        job: "burgershot",
     }
 ]
