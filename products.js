8a7ee2d3d69539f56d6fb60c3b75418625cb78fa
shanningtatum <94034582+shanningtatum@users.noreply.github.com>
2021-11-23 03:10:25 -0500

    Add files via upload

--- products.js
+++ products.js
@@ -110,3 +110,47 @@ const products = [
     },
 
 ]
+
+const services = [
+    {
+        id:0,
+        name: "Body Repair",
+        price:200.00,
+    },
+    {
+        id:1,
+        name: "Engine Repair",
+        price:800.00,
+    },
+    {
+        id:2,
+        name: "Lockpick",
+        price:1000.00,
+    },
+    {
+        id:3,
+        name: "Drill",
+        price:800.00,
+    },
+    {
+        id:4,
+        name: "Cloths",
+        price: 100.00
+    },
+    {
+        id:5,
+        name: "Adv. Lockpick",
+        price:1700.00
+    }
+]
+
+const logos = [
+    {   
+        // burgershot logo
+        imgSrc:"images/BSLogo.png",
+    },
+    {
+        // lost mc logo
+        imgSrc: "images/LostLogo.png",
+    }
+]
